821d62880bfd47932186cf540f4f4078e25bb6cb
kajugadaniels <kajugadaniels@gmail.com>
2024-04-24 14:39:22 +0200

    Folder structure

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from './Home'
+import Services from './Services'
+import Contact from './Contact'
+import Login from './Login'
+import Signup from './Signup'
+import Doctors from './Doctors/Doctors'
+import Details from './Doctors/Details'
+
+export {
+    Home,
+    Services,
+    Contact,
+    Login,
+    Signup,
+    Doctors,
+    Details
+}
