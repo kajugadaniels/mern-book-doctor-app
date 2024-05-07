019c54724cc8c972c9f2f4cd165a2625408bb96d
kajugadaniels <kajugadaniels@gmail.com>
2024-05-07 18:52:15 +0200

    Design services page

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { services } from "../assets/data/services.js";
+import { ServiceCard } from '../components/';
 
 const Services = () => {
     return (
-        <div>Services</div>
-    )
-}
+        <>
+            <section className="bg-[#fff9ea]">
+                <div className="container text-center">
+                    <h2 className="heading">Services</h2>
+                </div>
+            </section>
+            <section>
+                <div className="container">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+                        {services.map((item, index) => (
+                            <ServiceCard item={item} key={index} />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
 
-export default Services
+export default Services;
